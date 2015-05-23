9f213f9edaa770972e1ccd92da5849a398dcd2a6
Hoang-Minh Nguyen <alucard522@hotmail.com>
2015-05-23 15:43:52 -0500

    MultiTenancy Project

---
+++ MultiTenancy/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MultiTenancy
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add("DomainRoute", new DomainRoute(
+                "minh.us",                                     // Domain with parameters
+                "{action}/{id}",                                        // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            ));
+
+            routes.Add("DomainRoute2", new DomainRoute(
+                "{tenant}.minh.us",                                     // Domain with parameters
+                "{action}/{id}",                                        // URL with parameters
+                new { tenant = "subdomain", controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            ));
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MultiTenancy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MultiTenancy.Startup))]
+namespace MultiTenancy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
